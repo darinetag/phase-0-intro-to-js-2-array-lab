28875139ea950eda71ca2a488d79168d745d31ee
darinetag <tagdarinefatma@gmail.com>
2023-07-27 14:28:33 +0100

    Assignment checekd

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(Ralph){// Append the number 4 to the array
+    cats.push("Ralph") // Output: Updated Array: [1, 2, 3, 4]
+}
+function destructivelyPrependCat(Bob){
+    cats.unshift("Bob")
+}
+function destructivelyRemoveLastCat(remove){
+    cats.pop("Ralph")
+}
+function destructivelyRemoveFirstCat(first){
+    cats.shift("Milo")
+}
+function appendCat(Broom) {
+    const newCatsArray = [...cats]; 
+    newCatsArray.push("Broom"); 
+    return newCatsArray; 
+  }
+  function prependCat(Arnold) {
+    const newCatsArray = [...cats]; 
+    newCatsArray.unshift("Arnold"); 
+    return newCatsArray; 
+  }
+  function removeLastCat(Garfield){
+    const newCatsArray = [...cats];
+    newCatsArray.pop("Garfield")
+    return newCatsArray;
+  }
+  function removeFirstCat(Milo) {
+    const newCatsArray = [...cats]; 
+    newCatsArray.shift("Milo"); 
+    return newCatsArray; 
+  }
+  
+  
+
+
+
+
+
+
+//cats.push("Ralph")
+//console.log(cats)
